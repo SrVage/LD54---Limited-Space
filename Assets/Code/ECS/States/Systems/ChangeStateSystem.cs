3269b4db07d19f2c6730f8b276d0f3e4c669173b
Maksim_Krampit <savage_jawa@mail.ru>
2023-09-30 19:47:39 +0700

    States

---
+++ Assets/Code/ECS/States/Systems/ChangeStateSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Code.Abstract.Enums;
+using Code.ECS.States.Components;
+using Unity.Entities;
+
+namespace Code.ECS.States.Systems
+{
+    public partial struct ChangeStateSystem:ISystem
+    {
+        
+        public void OnCreate(ref SystemState state)
+        {
+            var stateEntity = state.EntityManager.CreateEntity();
+            state.EntityManager.AddComponent<MainState>(stateEntity);
+            state.EntityManager.AddComponent<MenuState>(stateEntity);
+            state.RequireForUpdate<ChangeState>();
+            state.RequireForUpdate<MainState>();
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var entity = SystemAPI.GetSingletonEntity<MainState>();
+            if (!SystemAPI.HasComponent<ExitState>(entity))
+            {
+                state.EntityManager.AddComponent<ExitState>(entity);
+                return;
+            }
+            var newState = SystemAPI.GetSingleton<ChangeState>().Value;
+            state.EntityManager.DestroyEntity(entity);
+            var stateEntity = state.EntityManager.CreateEntity();
+            state.EntityManager.AddComponent<MainState>(stateEntity);
+            state.EntityManager.AddComponent<EnterState>(stateEntity);
+            switch (newState)
+            {
+                case State.MenuState:
+                    state.EntityManager.AddComponent<MenuState>(stateEntity);
+                    break;
+                case State.PlayState:
+                    state.EntityManager.AddComponent<PlayState>(stateEntity);
+                    break;
+                case State.RechargeState:
+                    state.EntityManager.AddComponent<RechargeState>(stateEntity);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            state.EntityManager.DestroyEntity(SystemAPI.GetSingletonEntity<ChangeState>());
+        }
+    }
+}
