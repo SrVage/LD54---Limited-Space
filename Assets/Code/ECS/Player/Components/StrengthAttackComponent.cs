43429874c73e014e588807a445239a398607317d
Maksim_Krampit <savage_jawa@mail.ru>
2023-10-01 23:20:35 +0700

    PlayerAttack

---
+++ Assets/Code/ECS/Player/Components/StrengthAttackComponent.cs
@@ -0,0 +1,21 @@
+﻿using Code.Abstract.Interfaces;
+using Unity.Entities;
+
+namespace Code.ECS.Player.Components
+{
+    public struct StrengthAttackComponent:IComponentData, IEntityFeature
+    {
+        public int Value;
+        public float Distance;
+        public float Range;
+        public void Compose(IBaker baker, Entity entity)
+        {
+            baker.AddComponent(entity, this);
+        }
+
+        public void Compose(EntityCommandBuffer entityCommandBuffer, Entity entity)
+        {
+            entityCommandBuffer.AddComponent(entity, this);
+        }
+    }
+}
