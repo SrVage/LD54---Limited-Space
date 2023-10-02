d4908293fbf6cfb19ba5e33e8084a5edc3d83a7e
Maksim_Krampit <savage_jawa@mail.ru>
2023-10-02 14:14:08 +0700

    EnemyDamagePlayer

--- Assets/Code/ECS/Enemies/Attacking/EnemiesAttackingSystem.cs
+++ Assets/Code/ECS/Enemies/Attacking/EnemiesAttackingSystem.cs
@@ -1,6 +1,8 @@
 using Code.ECS.Enemies.Targeting;
 using Code.ECS.Moving;
+using Code.ECS.Player.Components;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 
@@ -18,6 +20,7 @@ namespace Code.ECS.Enemies.Attacking
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
             foreach (var (attackable, targetable, entity) in SystemAPI.Query<RefRW<AttackableComponent>, RefRO<TargetableComponent>>().WithEntityAccess())
             {
                 if (state.EntityManager.HasComponent<MovableComponent>(entity) && state.EntityManager.IsComponentEnabled<MovableComponent>(entity))
@@ -34,10 +37,16 @@ namespace Code.ECS.Enemies.Attacking
                 {
                     continue;
                 }
-                
-                Debug.Log("Damage");
+
+                ecb.AddComponent<HitComponent>(targetable.ValueRO.Target);
+                ecb.SetComponent(targetable.ValueRO.Target, new HitComponent()
+                {
+                    Value = attackable.ValueRO.Damage
+                });
                 attackable.ValueRW.CurrentCooldown = attackable.ValueRO.MaxCooldown;
             }
+            ecb.Playback(state.EntityManager);
+            ecb.Dispose();
         }
     }
 }
